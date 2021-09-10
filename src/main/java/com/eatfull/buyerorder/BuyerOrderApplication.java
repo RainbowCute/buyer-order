60490a886384529ad48d3be78a4e94d3122e2e5e
zhoumo <mo.zhou@thoughtworks.com>
2021-09-10 13:05:49 +0800

    [zhoumo] #N/A feat: init dependency

---
+++ src/main/java/com/eatfull/buyerorder/BuyerOrderApplication.java
@@ -0,0 +1,11 @@
+package com.eatfull.buyerorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyerOrderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BuyerOrderApplication.class, args);
+    }
+}
