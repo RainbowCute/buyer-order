756a1f52909c83cf2c0c0876f45f59e097e00ca3
zhoumo <mo.zhou@thoughtworks.com>
2021-09-12 18:48:45 +0800

    [zhoumo] Story1&AC3 feat: stub rabbit and send message exception, test return false

--- src/test/java/com/eatfull/buyerorder/message/MessageSenderTest.java
+++ src/test/java/com/eatfull/buyerorder/message/MessageSenderTest.java
@@ -4,11 +4,15 @@ import com.eatfull.buyerorder.base.IntegrationTest;
 import com.eatfull.buyerorder.message.dto.OrderCancellationMessage;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.springframework.amqp.AmqpException;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.util.ReflectionTestUtils;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 
 public class MessageSenderTest extends IntegrationTest {
 
@@ -25,4 +29,14 @@ public class MessageSenderTest extends IntegrationTest {
         assertTrue(send);
     }
 
+    @Test
+    void should_return_true_whe_send_message_given_rabbit_template_throw_exception() {
+        RabbitTemplate stubRabbitTemplate = Mockito.mock(RabbitTemplate.class);
+        ReflectionTestUtils.setField(messageSender, "rabbitTemplate", stubRabbitTemplate);
+        Mockito.doThrow(AmqpException.class).when(stubRabbitTemplate).convertAndSend(anyString(), anyString(), java.util.Optional.ofNullable(any()));
+
+        boolean send = messageSender.send(OrderCancellationMessage.builder().orderId(1L).build());
+
+        assertFalse(send);
+    }
 }
