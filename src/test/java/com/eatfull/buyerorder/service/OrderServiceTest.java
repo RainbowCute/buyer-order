7352395ecd1f30f8436a2f853b39545906913283
zhoumo <mo.zhou@thoughtworks.com>
2021-09-13 09:20:05 +0800

    [zhoumo] Story2&AC3 feat: stub stock feign client throw exception, test service order throw order stock service unavailable exception

--- src/test/java/com/eatfull/buyerorder/service/OrderServiceTest.java
+++ src/test/java/com/eatfull/buyerorder/service/OrderServiceTest.java
@@ -11,10 +11,12 @@ import com.eatfull.buyerorder.infrastructure.entity.MessageHistory;
 import com.eatfull.buyerorder.infrastructure.entity.OrderItem;
 import com.eatfull.buyerorder.infrastructure.exceptions.OrderCancelFailedException;
 import com.eatfull.buyerorder.infrastructure.exceptions.OrderCreationFailedException;
+import com.eatfull.buyerorder.infrastructure.exceptions.StockServiceUnavailableException;
 import com.eatfull.buyerorder.infrastructure.repository.MessageHistoryRepository;
 import com.eatfull.buyerorder.infrastructure.repository.OrderRepository;
 import com.eatfull.buyerorder.message.MessageSender;
 import com.eatfull.buyerorder.model.OrderModel;
+import feign.FeignException;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -169,5 +171,16 @@ public class OrderServiceTest extends IntegrationTest {
 
         OrderCreationFailedException orderCreationFailedException = assertThrows(OrderCreationFailedException.class, () -> orderService.createOrder(orderModel));
         assertEquals("库存不足", orderCreationFailedException.getMessage());
+    }
+
+    @Test
+    void should_throw_exception_when_create_order_given_stock_service_unavailable_exception() {
+        StockClient stubStockClient = Mockito.mock(StockClient.class);
+        ReflectionTestUtils.setField(orderService, "stockClient", stubStockClient);
+        Mockito.when(stubStockClient.reserve(any())).thenThrow(FeignException.class);
+        OrderModel orderModel = OrderModel.builder().orderItemModels(new ArrayList<>()).build();
+
+        StockServiceUnavailableException stockServiceUnavailableException = assertThrows(StockServiceUnavailableException.class, () -> orderService.createOrder(orderModel));
+        assertEquals("库存服务不可用，请稍后再试", stockServiceUnavailableException.getMessage());
     }
 }
